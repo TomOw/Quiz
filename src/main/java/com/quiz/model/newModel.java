3d7882e215f161c62a9ec0d98617fb781fc32057
TomOw <owczarczyk.tomasz@wp.pl>
2016-08-29 22:19:02 +0200

    added newModel

---
+++ src/main/java/com/quiz/model/newModel.java
@@ -0,0 +1,21 @@
+package com.quiz.model;
+
+/**
+ * Created by Tomasz on 29.08.2016.
+ */
+public class newModel {
+
+    private int x;
+
+    public newModel(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
